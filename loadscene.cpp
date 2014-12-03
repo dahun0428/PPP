55c2aed2a26c23c54a0019bff7531ef0069a874c
ytbeom <ytbeom@gmail.com>
2014-12-04 05:40:29 +0900

    load test

--- loadscene.cpp
+++ loadscene.cpp
@@ -115,7 +115,7 @@ void LoadScene::clickButton1()
     if(load[0]) {
         // game load
         Game* pg = getGameClass();
-        pg->loadGame("..\\..\\PPP\\Resources\\Save\\save_data1");
+        pg->saveGame("C:\\Qt\\Tools\\QtCreator\\bin\\PPP\\Resources\\Save\\save_data1");
         nextScene = new NewScene( getGameClass() );
     }
 }
